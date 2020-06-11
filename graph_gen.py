e1f002f1a16bb54b12d85b4a93e2a077630a4db3
Rosen Popov <rdonkow@gmail.com>
2020-06-12 02:44:05 +0300

    Added a script to generate graphics Added docimentation

--- graph_gen.py
+++ graph_gen.py
@@ -1,39 +1,99 @@
 import csv
-import pandas
 import sys
 import os 
+import numpy as np
+import matplotlib.pyplot as plt
 
 def read_csv(name):
-    data  = [] 
+    data  = []
+    mean_time=[]
     with open(name, newline='') as f:
         reader = csv.reader(f)
         data = list(reader)
         data.pop(0)
-    print("data",data)
-    return data
+    for i in range(len(data)):
+        mean_time.append(int(data[i][len(data[i])-1].strip(';')))
+    return np.mean(mean_time)
 
 
-def gen_graph(listo):
+def gen_graph(listo,n_thr):
+    size_of_input_buffer = [512, 1024, 2048, 4096, 8192, 12288, 16384, 20480, 24576, 32768]
     tmp_list = []
-    print(tmp_list)
-    print ("appending ", listo[0])
-    tmp_list.append(read_csv(listo[0]))
-    print(tmp_list)
+    listo.sort()
+    for i in range(len(listo)):
+        tmp = read_csv(listo[i])
+        tmp_list.append(tmp)
+    plt.plot(tmp_list,size_of_input_buffer)
+    plt.xlabel("Mean thread execution time (Threads: {})".format(str(n_thr)))
+    plt.ylabel("Read buffer size")
+    print("made graph for {} thread".format(n_thr))
+    plt.savefig("threads" + str(n_thr) + ".png",figsize=(1920,1020))
+    plt.close()
+    return tmp_list
+
+
 
 def get_names_of_files(name_wild, dir_thr):
     path = './res/'+ str(dir_thr) + '/'
     files = []
-    print(name_wild)
     for r, d, f in os.walk(path):
         for file in f:
             if name_wild in file:
                 files.append(os.path.join(r, file))
     return files
 
+def generate_all_graphs(name,threads):
+    final_info = []
+    size_of_input_buffer = [512, 1024, 2048, 4096, 8192, 12288, 16384, 20480, 24576, 32768]
+    colors = [   "#178b90",
+                 "#2586a2",
+                 "#29d5cb",
+                 "#2eadd0",
+                 "#37c6ed",
+                 "#3e550f",
+                 "#413616",
+                 "#4c221d",
+                 "#586bd5",
+                 "#619e9a",
+                 "#6399fb",
+                 "#6e45ef",
+                 "#7531d3",
+                 "#7f78a4",
+                 "#86f4dd",
+                 "#8d4580",
+                 "#8fb7ac",
+                 "#97e31a",
+                 "#a01f99",
+                 "#ae009f",
+                 "#af8855",
+                 "#b6b44d",
+                 "#ba916a",
+                 "#cc065c",
+                 "#cc20c2",
+                 "#d8b4f3",
+                 "#f62886"]
+    for i in range(threads):
+        res = get_names_of_files('bench-results-' + name + '-',i+1 )
+        res.sort()
+        final_info.append( gen_graph(res,i+1))
 
-sys.argv.pop(0) 
-print("fname = ",sys.argv[0])
-#for i in range(16):
-res = get_names_of_files('bench-results-' + sys.argv[0] + '-',1 )
-res.sort()
-gen_graph(res)
+    for i in range(threads):
+        plt.plot(size_of_input_buffer,final_info[i], label ="Threads {}".format(i+1))
+
+    plt.legend(loc="upper right", ncol=3)
+    plt.title("All threads plot")
+    plt.xlabel("Input_size")
+    plt.ylabel("Time")
+    plt.savefig("All threads Fig",figsize=(1920,1080))
+
+sys.argv.pop(0)
+name = sys.argv[0]
+threads = int(sys.argv[1])
+if threads == 0 :
+   threads = 16
+if name == "":
+    name = "lorem"
+threads=16
+name="lorem"
+generate_all_graphs(name, threads)
+
